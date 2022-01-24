97303bc367a9decb205a9d7f35b17c669718c1e4
Narcis Nogue <narcis.nogue@spi-web.org>
2022-01-24 10:03:41 +0100

    Avançar frontend i crear backend

--- frontend/src/components/board.js
+++ frontend/src/components/board.js
@@ -7,9 +7,10 @@ import Utils from "../utils.js"
 function Board() {
   const [boardState, setBoardState] = useState(Array(9).fill(null))
   const [player, setPlayer] = useState(null);
+  const [playerName, setPlayerName] = useState(null);
 
   const onClickSquare = (square) => {
-    if(player === null) return;
+    if(player === null || boardState[square] !== null) return;
     let board = boardState.slice();
     board = Utils.onClickSquare(board, square, player);
     setBoardState(board);
@@ -18,16 +19,25 @@ function Board() {
   const onSelectPlayer = (pl) => {
     if(player) return;
     setPlayer(pl);
+    if(pl === "O") { //If we are O, computer plays first
+      let board = boardState.slice();
+      board = Utils.playIA(board, pl);
+      setBoardState(board);
+    }
+  }
+
+  const onChangeName = (e) => {
+    setPlayerName(e.target.value);
   }
 
   return (
     <div className="game">
-      <StartScreen click={onSelectPlayer} currPlayer={player}/>
+      <StartScreen click={onSelectPlayer} onChangeName={onChangeName} currPlayer={player} currPlayerName={playerName}/>
       <p>{player ? "Jugando como " + player : "Selecciona jugador"}</p>
       <div className="board">
         {  
           boardState.map((square, index) => {
-            return < Square key={index} value={square} index={index} click={onClickSquare}/>
+            return < Square key={index} value={square} index={index} click={onClickSquare} />
           })
         }
       </div>
--- frontend/src/components/startscreen.js
+++ frontend/src/components/startscreen.js
@@ -3,8 +3,9 @@ import "./styles/startscreen.css"
 function StartScreen(props) {
   return (
       <div className="startScreen">
-        <button className="startScreenButton" onClick={() => {props.click("X")}} disabled={props.currPlayer != null}>Jugar como X</button>
-        <button className="startScreenButton" onClick={() => {props.click("O")}} disabled={props.currPlayer != null}>Jugar como O</button>
+        <input className="startScreenNameInput" onChange={(e) => {props.onChangeName(e)}} disabled={props.currPlayer !== null}></input>
+        <button className="startScreenButton" onClick={() => {props.click("X")}} disabled={props.currPlayer !== null || props.currPlayerName === null}>Jugar como X</button>
+        <button className="startScreenButton" onClick={() => {props.click("O")}} disabled={props.currPlayer !== null || props.currPlayerName === null}>Jugar como O</button>
       </div>
     );
 }
